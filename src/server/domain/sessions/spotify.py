dbda7f9c8bfaef51ae851dcba3edd022e794273b
robert-huang <robert054321@hotmail.com>
2024-06-20 01:17:13 -0400

    custom memegen image for local files

--- src/server/domain/sessions/spotify.py
+++ src/server/domain/sessions/spotify.py
@@ -1,7 +1,11 @@
 import json
 import time
 import itertools
+import random
+import re
 import requests
+from google_images_search import GoogleImagesSearch
+import googleapiclient
 from util.logging import GLOBAL_LOGGER as logger
 from util.env_vars import getEnvironmentVariable
 from util.requests import getRequest, postRequest
@@ -37,15 +41,17 @@ class Spotify:
     __clientSecret: str = None
     __accessToken: str = None
     __tokenExpiry: float = None
-    
+
     def __init__(self) -> None:
         self.database = SpotifyDataBase()
         self.songCache = {}
         self.artistCache = {}
         self.__clientId = getEnvironmentVariable("SPOTIFY_CLIENT_ID")
         self.__clientSecret = getEnvironmentVariable("SPOTIFY_CLIENT_SECRET")
+        self.__googleCustomSearchKey = getEnvironmentVariable("GOOGLE_CUSTOM_SEARCH_DEV_KEY", False)
+        self.__googleCustomSearchCX = getEnvironmentVariable("GOOGLE_CUSTOM_SEARCH_CX", False)
         self.__generateAccessToken()
-    
+
     def getPlaylistTracks(self, playlistId: str) -> any:
         token = self.__getAccessToken()
         batches = []
@@ -63,18 +69,18 @@ class Spotify:
                     break
                 else:
                     requestUrl = batch["next"]
-            except requests.exceptions.HTTPError as errh: 
+            except requests.exceptions.HTTPError as errh:
                 if (errh.response.status_code == 404):
                     raise SpotifyPlaylistNotFoundException(playlistId)
                 else:
                     raise SpotifyPlaylistRetrievalException(playlistId)
             except Exception as e:
                 raise e
-            
+
         return {
             "items": list(itertools.chain(*batches))
         }
-    
+
     def multipleArtistQuery(self, ids: str) -> any:
         idBatches = self.batchIds(ids)
         batches = []
@@ -90,20 +96,20 @@ class Spotify:
                 ).json()["artists"]
                 batches.append(batch)
 
-            except requests.exceptions.HTTPError as errh: 
+            except requests.exceptions.HTTPError as errh:
                 raise SpotifyArtistRetrievalException(idList)
             except Exception as e:
                 raise e
-            
+
         return {
             "artists": list(itertools.chain(*batches))
         }
-    
+
     def batchIds(self, ids: str, batchSize: int = 50):
         idList = ids.split(",")
         for i in range(0, len(idList), batchSize):
             yield idList[i:i + batchSize]
-    
+
     def __getAccessToken(self) -> str:
         if self.__accessTokenHasExpired():
             self.__generateAccessToken()
@@ -136,7 +142,7 @@ class Spotify:
         if (isExpired):
             logger.info(f"Spotify access token is expired. Now: {now}, Expiry: {self.__tokenExpiry} ({self.__tokenExpiry - now} seconds)")
         return isExpired
-    
+
     def addSong(self, songs: list[dict]) -> None:
         newSongs = self.database.addSpotifySongs(songs)
         for song in newSongs:
@@ -153,7 +159,7 @@ class Spotify:
             else:
                 # logger.debug(f"Found '{id}' in Spotify song cache.")
                 requestedSongs.append(self.songCache.get(id).getMap())
-        
+
         dbList = self.database.getSpotifySongs(notCached)
 
         for spotifySong in dbList:
@@ -162,6 +168,39 @@ class Spotify:
             # logger.debug(f"Added missing to Spotify song cache: '{song.id}'")
 
         return requestedSongs
+
+    def __getCustomSongImage(song: SpotifySong) -> str:
+        query = song.album if song.album else ' '.join([song.name, song.artists])
+
+        _search_params={
+            'q': query,
+            'num': 1
+        }
+
+        album_cover_url = ''
+        try:
+            gis = GoogleImagesSearch(this.__googleCustomSearchKey, this.__googleCustomSearchCX)
+            gis.search(search_params=_search_params)
+            for image in gis.results():
+                album_cover_url = __clean_google_image_url(image.url)
+        except googleapiclient.errors.HttpError as e:
+            if e.resp.status == 429:
+                album_cover_url = 'https://i.imgur.com/Gvgmuwr.jpg'
+            else:
+                raise e
+
+        background = 'https://raw.githubusercontent.com/AlexPolGit/Super-Sorter/main/src/client/src/assets/spotify-empty-song.jpg'
+
+        custom_song_image_url = f'https://api.memegen.link/images/custom/_/{song.artists}~n{song.name}~n{song.album}~n~n%C2%A0.png?background={background}&font=jp&center=.5%2C.324&scale=0.49&style={album_cover_url}'
+
+        return custom_song_image_url
+
+    def __clean_google_image_url(url: str) -> str:
+        if re.search('media-amazon', url):
+            re_split = re.split('(.*\.).*?\.(jpg|png|gif|jpeg|svg|webp)', url)
+            return ''.join(re_split)
+        else:
+            return url
 
     def addArtists(self, artists: list[dict]) -> None:
         newArtists = self.database.addSpotifyArtists(artists)
@@ -179,7 +218,7 @@ class Spotify:
             else:
                 # logger.debug(f"Found '{id}' in Spotify artist cache.")
                 requestedArtists.append(self.artistCache.get(id).getMap())
-        
+
         dbList = self.database.getSpotifyArtists(notCached)
 
         for spotifyArtist in dbList:
